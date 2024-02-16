e724d2283fca4bccf56c5846452c0168618c2f9d
jdcarrothers <jdcarrothers@icloud.com>
2024-02-16 11:24:43 +0000

    fixing vue config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: [
-    'path-browserify',
-    'crypto-browserify',
-    'stream-browserify',
-    'browserify-zlib',
-    'querystring-es3',
-    'stream-http'
-  ],
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://dev-duel-alpha.vercel.app',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    }
-  }
+  
+  
 });
